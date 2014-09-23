0588e75db419e3741b48cdd41a60a178d6d10c21
Trevor Brown <tbrown@voalte.com>
2014-09-23 13:08:28 -0400

    Working on adding the assertion functions to the teal_modules module.

--- src/teal_modules.erl
+++ src/teal_modules.erl
@@ -1,6 +1,8 @@
 -module(teal_modules).
 
--export([exports/2, exports/3]).
+-export([exports/2, assert_exports/2, assert_exports/3,
+         exports_with_arity/3, assert_exports_with_arity/3,
+         assert_exports_with_arity/4]).
 
 %%%===================================================================
 %%% API
@@ -20,10 +22,22 @@ exports(Module, Function) ->
                 end
         end, Exports).
 
--spec exports(Module :: atom(), Function :: atom(), Arity :: integer()) ->
+-spec assert_exports(Module :: atom(), Function :: atom()) ->
     boolean().
 
-exports(Module, Function, Arity) ->
+assert_exports(Module, Function) ->
+    teal:assert(true, exports(Module, Function), function_not_exported).
+
+-spec assert_exports(Module :: atom(), Function :: atom(), Msg :: atom()) ->
+    boolean().
+
+assert_exports(Module, Function, Msg) ->
+    teal:assert(true, exports(Module, Function), Msg).
+
+-spec exports_with_arity(Module :: atom(), Function :: atom(), Arity :: integer()) ->
+    boolean().
+
+exports_with_arity(Module, Function, Arity) ->
     Exports = module_exports(Module),
     lists:any(fun(Export) ->
                 case Export of
@@ -33,6 +47,19 @@ exports(Module, Function, Arity) ->
                         false
                 end
         end, Exports).
+
+-spec assert_exports_with_arity(Module :: atom(), Function :: atom(),
+                                Arity :: integer()) -> boolean().
+
+assert_exports_with_arity(Module, Function, Arity) ->
+    teal:assert(true, exports_with_arity(Module, Function, Arity),
+                function_not_exported).
+
+-spec assert_exports_with_arity(Module :: atom(), Function :: atom(),
+                                Arity :: integer(), Msg :: atom()) -> boolean().
+
+assert_exports_with_arity(Module, Function, Arity, Msg) ->
+    teal:assert(true, exports_with_arity(Module, Function, Arity), Msg).
 
 %%%===================================================================
 %%% Private functions
