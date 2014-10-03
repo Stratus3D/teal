5165ccd9e9c78ae1d5d92ec6a8aa1a5553170cff
Trevor Brown <tbrown@voalte.com>
2014-10-03 09:36:00 -0400

    Created the has_callback function in the teal_behaviours module.

--- src/teal_behaviours.erl
+++ src/teal_behaviours.erl
@@ -1,15 +1,27 @@
 -module(teal_behaviours).
 
--export([is_behaviour/1]).
+-export([has_callback/3,
+    is_behaviour/1]).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
+-spec has_callback(Module :: atom(), Name :: atom(), Arity :: integer()) ->
+    boolean().
+
+has_callback(Module, Name, Arity) ->
+    Callbacks = get_callbacks(Module),
+    lists:any(fun({callback, Details}) ->
+                [CallbackDetails] = Details,
+                {{CallbackName, CallbackArity}, _Args} = CallbackDetails,
+                ({CallbackName, CallbackArity} == {Name, Arity})
+        end, Callbacks).
+
 -spec is_behaviour(Module :: atom()) -> boolean().
 
 is_behaviour(Module) ->
-    case get_behaviours(Module) of
+    case get_callbacks(Module) of
         [] ->
             false;
         _ ->
@@ -20,7 +32,7 @@ is_behaviour(Module) ->
 %%% Private functions
 %%%===================================================================
 
-get_behaviours(Module) ->
+get_callbacks(Module) ->
     Attributes = Module:module_info(attributes),
     lists:filter(fun({AttrName, _Opts}) ->
                 case AttrName of
