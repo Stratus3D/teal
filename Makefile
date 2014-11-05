06f7cc56600a6558c6a7dd56de2caff425266dd7
Trevor Brown <tbrown@voalte.com>
2014-11-04 19:24:25 -0500

    Wrote test module for the teal_types module.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 PROJECT = teal
 
-CT_SUITES = teal teal_lists teal_modules teal_processes teal_behaviours
+CT_SUITES = teal teal_lists teal_modules teal_processes teal_behaviours \
+			teal_types
 
 include erlang.mk
---
+++ test/teal_types_SUITE.erl
@@ -0,0 +1,59 @@
+-module(teal_types_SUITE).
+
+%% API
+-export([all/0,
+         suite/0,
+         groups/0,
+         init_per_suite/1,
+         end_per_suite/1,
+         group/1,
+         init_per_group/2,
+         end_per_group/2,
+         init_per_testcase/2,
+         end_per_testcase/2]).
+
+%% Test cases
+-export([test_not_record/1]).
+
+-include_lib("common_test/include/ct.hrl").
+
+%%%===================================================================
+%%% Common Test callbacks
+%%%===================================================================
+
+all() ->
+    [test_not_record].
+
+suite() ->
+    [{timetrap, {seconds, 30}}].
+
+groups() ->
+    [].
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+group(_GroupName) ->
+    [].
+
+init_per_group(_GroupName, Config) ->
+    Config.
+
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%%===================================================================
+%%% Test cases
+%%%===================================================================
+
+test_not_record(_Config) ->
+    ok.
