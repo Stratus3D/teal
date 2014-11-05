2d7ba54cc4f5fbdc854ae91ec5ee74bbe490810f
Trevor Brown <tbrown@voalte.com>
2014-11-05 07:27:19 -0500

    Wrote tests for the assert_not_record/1 and assert_not_record/2 functions.

--- test/teal_types_SUITE.erl
+++ test/teal_types_SUITE.erl
@@ -13,7 +13,8 @@
          end_per_testcase/2]).
 
 %% Test cases
--export([test_not_record/1]).
+-export([test_not_record/1, test_assert_not_record_1/1,
+         test_assert_is_module_2/1]).
 
 -include_lib("common_test/include/ct.hrl").
 
@@ -22,7 +23,7 @@
 %%%===================================================================
 
 all() ->
-    [test_not_record].
+    [test_not_record, test_assert_not_record_1, test_assert_is_module_2].
 
 suite() ->
     [{timetrap, {seconds, 30}}].
@@ -62,3 +63,32 @@ test_not_record(_Config) ->
     % Should return true when the term cannot be a record
     true = teal_types:not_record({[], a}),
     true = teal_types:not_record(not_a_record).
+
+test_assert_not_record_1(_Config) ->
+    % Should return true when the term cannot be a record
+    true = teal_types:assert_not_record({[], a}),
+    true = teal_types:assert_not_record(not_a_record),
+
+    % Should raise an error when the term looks like a record
+    try teal_types:assert_not_record({foo, bar}) of
+        _ -> erlang:error(failed)
+    catch
+        error:is_a_record ->
+            true
+    end.
+
+test_assert_is_module_2(_Config) ->
+    Msg = test,
+
+    % Should return true when the term cannot be a record
+    true = teal_types:assert_not_record({[], a}, Msg),
+    true = teal_types:assert_not_record(not_a_record, Msg),
+
+    % Should raise an error when the term looks like a record
+    try teal_types:assert_not_record({foo, bar}, Msg) of
+        _ -> erlang:error(failed)
+    catch
+        error:Msg ->
+            true
+    end.
+
