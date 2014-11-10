9e666068fab9ed8488983aa8e727c716e96b1eaa
Trevor Brown <tbrown@voalte.com>
2014-11-10 09:25:26 -0500

    Wrote the is_registered/1 function in the teal_processes module.

--- src/teal_processes.erl
+++ src/teal_processes.erl
@@ -1,1 +1,22 @@
 -module(teal_processes).
+
+-export([is_registered/1]).
+
+
+-spec is_registered(Process :: pid() | atom()) -> boolean().
+
+is_registered(Process) when is_atom(Process) ->
+    case erlang:whereis(Process) of
+        undefined ->
+            false;
+        _ ->
+            true
+    end;
+
+is_registered(Process) when is_pid(Process) ->
+    case erlang:process_info(Process, [registered_name]) of
+        [{registered_name, []}] ->
+            false;
+        _ ->
+            true
+    end.
