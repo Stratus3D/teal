da331422fb8168453b560578092766bb89deceaf
Stratus3D <admin@stratus3d.com>
2015-01-12 07:42:01 -0500

    Wrote test for the assert_close_to/3 function.

--- test/teal_numbers_SUITE.erl
+++ test/teal_numbers_SUITE.erl
@@ -13,7 +13,8 @@
          end_per_testcase/2]).
 
 %% Test cases
--export([test_close_to/1]).
+-export([test_close_to/1,
+        test_assert_close_to_2/1]).
 
 -include_lib("common_test/include/ct.hrl").
 
@@ -22,7 +23,8 @@
 %%%===================================================================
 
 all() ->
-    [test_close_to].
+    [test_close_to,
+    test_assert_close_to_2].
 
 suite() ->
     [{timetrap, {seconds, 30}}].
@@ -65,4 +67,21 @@ test_close_to(_Config) ->
 
     % Should return false when the input number is outside of the range
     AnotherTarget = 10,
-    true = teal_numbers:close_to(Input, AnotherTarget, Delta).
+    false = teal_numbers:close_to(Input, AnotherTarget, Delta).
+
+test_assert_close_to_2(_Config) ->
+    Delta = 2,
+    Target = 2,
+    Input = 1,
+
+    % Should return true when the input number is inside the range
+    true = teal_numbers:assert_close_to(Input, Target, Delta),
+
+    % Should raise an error when the input number is outside of the range
+    AnotherTarget = 10,
+    try teal_numbers:assert_close_to(Input, AnotherTarget, Delta) of
+        _ -> erlang:error(failed)
+    catch
+        error:not_in_range ->
+            true
+    end.
